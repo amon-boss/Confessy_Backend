0f9641d9918794fe7c6cc3f212584bd4bb95bfc0
amon-boss <66650142+amon-boss@users.noreply.github.com>
2025-06-29 05:10:00 +0000

    Create server.js

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+
+const authRoutes = require('./routes/authRoutes');
+const confessionRoutes = require('./routes/confessionRoutes');
+
+dotenv.config();
+connectDB();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/auth', authRoutes);
+app.use('/api/confessions', confessionRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`🚀 Serveur lancé sur le port ${PORT}`));
