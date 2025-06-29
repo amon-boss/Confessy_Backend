bf47adcaf0d6008b0505f1428865ea17f544c055
amon-boss <66650142+amon-boss@users.noreply.github.com>
2025-06-29 05:04:49 +0000

    Update Confession.js

--- models/Confession.js
+++ models/Confession.js
@@ -1,12 +1,11 @@
 const mongoose = require('mongoose');
 
 const confessionSchema = new mongoose.Schema({
-  userId: String,
-  username: String,
-  anonymous: Boolean,
-  content: String,
-  likes: [String],
-  createdAt: { type: Date, default: Date.now }
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  content: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now },
+  likesCount: { type: Number, default: 0 },
+  commentsCount: { type: Number, default: 0 }
 });
 
 module.exports = mongoose.model('Confession', confessionSchema);
