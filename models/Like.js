0004af460dbf6944f4b0592167a039576afd5b40
amon-boss <66650142+amon-boss@users.noreply.github.com>
2025-06-29 05:38:09 +0000

    Delete models/Like.js

--- models/Like.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const likeSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-  confessionId: { type: mongoose.Schema.Types.ObjectId, ref: 'Confession' }
-});
-
-module.exports = mongoose.model('Like', likeSchema);
