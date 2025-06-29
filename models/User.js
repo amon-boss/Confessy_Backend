29aa9c6b7c849018832f5bf489b81776a034c385
amon-boss <66650142+amon-boss@users.noreply.github.com>
2025-06-29 05:04:08 +0000

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  contact: { type: String, unique: true },
-  password: String,
-  username: { type: String, unique: false },
-  isAnonymous: Boolean
+  emailOrPhone: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  username: { type: String },
+  isAnonymous: { type: Boolean, default: false }
 });
 
 module.exports = mongoose.model('User', userSchema);
